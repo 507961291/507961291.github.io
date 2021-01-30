7ad2e6fe18a2eb36f1fcdcd7826c173318edc91f
507961291 <bus20001@byui.edu>
2021-01-30 00:49:20 -0800

    lesson 4

---
+++ lesson_4/js/script.js
@@ -0,0 +1,22 @@
+ //current date
+ const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+ document.getElementById("current").textContent= new Date().toLocaleDateString("en-US", options);
+
+  // Copyright Year
+  let cyear = new Date();
+  let year = cyear.getFullYear();
+  document.getElementById("cYear").textContent= year;
+
+  //Toggle Menu
+ function toggleMenu(){
+    var nav = document.querySelector('nav ul');
+    var menuButton = document.querySelector('.navMenuButton');
+    var display = document.querySelector('.responsive');
+    nav.classList.toggle('responsive'), false;
+    if (display!=null){
+        menuButton.innerHTML='<a href="#">&equiv;</a>';}
+    else {
+        menuButton.innerHTML='<a href="#">^</a>';}
+ }
+
+ //window.onresize=() => {if (window.innerWidth >760) mainnav.classlist.remove('responsive');
