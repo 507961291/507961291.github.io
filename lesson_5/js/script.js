1362c3ac18c8c3c8aed31c69fa9060c730fccbb7
507961291 <bus20001@byui.edu>
2021-02-03 23:16:41 -0800

    lesson 5

---
+++ lesson_5/js/script.js
@@ -0,0 +1,25 @@
+
+window.onresize = () => {var nav = document.querySelector('nav ul');if (window.innerWidth > 760) nav.classList.remove('.responsive')};
+
+//current date
+ const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+ document.getElementById("current").textContent= new Date().toLocaleDateString("en-US", options);
+
+  // Copyright Year
+  let cyear = new Date();
+  let year = cyear.getFullYear();
+  document.getElementById("cYear").textContent= year;
+
+  //Toggle Menu
+ function toggleMenu(){
+    var nav = document.querySelector('nav ul');
+    var menuButton = document.querySelector('.navMenuButton');
+    var display = document.querySelector('.responsive');
+    nav.classList.toggle('responsive'), false;
+    if (display!=null){
+        menuButton.innerHTML='<a href="#">&equiv;</a>';}
+    else {
+        menuButton.innerHTML='<a href="#">^</a>';}
+ }
+
+ 
