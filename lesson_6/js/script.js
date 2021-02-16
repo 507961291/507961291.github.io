b9b83f5d7dbe553b175a613eead49f3975a05837
507961291 <bus20001@byui.edu>
2021-02-15 22:34:22 -0800

    lesson 6

--- lesson_6/js/script.js
+++ lesson_6/js/script.js
@@ -1,11 +1,28 @@
 
 window.onresize = () => {var nav = document.querySelector('nav ul');if (window.innerWidth > 760) nav.classList.remove('.responsive')};
 
+//Font Loader
+ WebFont.load({
+    google: {
+      families: [
+         'Raleway', 'Poller One'
+      ]
+    }
+  });
+
+  //Wind Chill calculation
+let f = 30; /*current temperature in degrees Fahrenheit*/
+let ws = 5; /* windspeed in miles per hour*/
+let wc = 35.74 + 0.6215 + f - 35.75 * Math.pow(ws, 0.16) + 0.4275 * f * Math.pow(ws,0.16);
+document.getElementById('high_temp').innerText=f.toFixed(1)+"° Fahrenheit";
+document.getElementById('wind_chill').innerText= wc.toFixed(1)+"° Fahrenheit";
+
+
 //current date
  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
  document.getElementById("current").textContent= new Date().toLocaleDateString("en-US", options);
 
-  // Copyright Year
+  // Copyright Year & Find Friday
   let now = new Date();
   let year = now.getFullYear();
   document.getElementById("cYear").textContent= year;
