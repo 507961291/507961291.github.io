5ef4c517395e17b6854c77d095f3c6c3bbc2b8cc
507961291 <bus20001@byui.edu>
2021-03-13 22:37:51 -0800

    lesson 10

--- lesson_10/js/script.js
+++ lesson_10/js/script.js
@@ -38,3 +38,35 @@ window.onresize = () => {var nav = document.querySelector('nav ul');if (window.i
         nav.style.height="100%";
       }  
  }
+
+ //weather api
+ const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=5cf8b8a6ee5cb33fce30431f6f49c46&units=imperial";
+
+ fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+
+    document.getElementById('weather_desc').textContent =jsObject.weather[0].main;
+    document.getElementById('high_temp').textContent = jsObject.main.temp_max;
+    document.getElementById('wind_chill').textContent = (windChill(jsObject.main.temp, jsObject.wind.speed)).toFixed(2);
+    document.getElementById('humidity').textContent =jsObject.main.humidity;
+    document.getElementById('wind_speed').textContent =jsObject.wind.speed;
+
+  })
+
+  function windChill(t, s){
+    let f = 35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16);
+    return f;	
+  }
+
+  //5 day forecast
+  const apiURL2 = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=5cf8b8a6ee5cb33fce30431f6f49c46&units=imperial";
+
+  fetch(apiURL2)
+    .then((response)=> response.json())
+    .then((jsObject2)=> {
+      console.log(jsObject2);
+
+      
+    })
